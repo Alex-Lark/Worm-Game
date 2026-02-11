8dd6fd02b3e98f861c48ebe822531f3e94646bd6
Alex Lark <lark@chapman.edu>
2026-02-10 21:41:13 -0800

    added chat

--- Assets/Scripts/GameLoop/GameLobby.cs
+++ Assets/Scripts/GameLoop/GameLobby.cs
@@ -1,11 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace GameLoop
 {
     public class GameLobby : MonoBehaviour
     {
+        #region Public Variables
+        
         public Material wormMaterial;
         public Material wormHeadMaterial;
         
@@ -13,14 +16,28 @@ namespace GameLoop
         public Image playerList;
 
         public GameObject colorSelectionPanel;
+        public Transform chatImage;
 
         public GameObject playerUsernameTextPrefab;
+        public GameObject chatTextPrefab;
+        
+        public TMP_InputField chatInputField;
+
+        public int maxMessages = 10;
+        
+        #endregion
+        
+        #region Built-In Methods
 
         void Start()
         {
             selectColorButtonColor.color = wormMaterial.color;
             UpdatePlayerList();
         }
+        
+        #endregion
+        
+        #region Public Methods
 
         public void OpenColorSelectionPanel()
         {
@@ -62,5 +79,34 @@ namespace GameLoop
                 tmpText.text = player.PlayerName;
             }
         }
+
+        public void SendChatMessage(string message)
+        {
+            if (message == "")
+            {
+                return;
+            }
+            
+            string finalMessage = "<" + Player.Player.Instance.PlayerName + "> " + message;
+            
+            GameObject messageObject = Instantiate(chatTextPrefab, chatImage);
+            TextMeshProUGUI messageText = messageObject.GetComponent<TextMeshProUGUI>();
+            messageText.text = finalMessage;
+            
+            if (chatImage.childCount > maxMessages)
+            {
+                Destroy(chatImage.GetChild(0).gameObject);
+            }
+            
+            chatInputField.text = "";
+            chatInputField.ActivateInputField();
+        }
+
+        public void DeactivateInputField()
+        {
+            chatInputField.DeactivateInputField();
+        }
+        
+        #endregion
     }
 }
