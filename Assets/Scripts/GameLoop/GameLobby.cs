6908b919eadb3267b20fb1157ecce13588f0877f
Alex Lark <lark@chapman.edu>
2026-02-10 19:52:45 -0800

    color selection works

--- Assets/Scripts/GameLoop/GameLobby.cs
+++ Assets/Scripts/GameLoop/GameLobby.cs
@@ -1,12 +1,49 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace GameLoop
 {
     public class GameLobby : MonoBehaviour
     {
+        public Material wormMaterial;
+        public Material wormHeadMaterial;
+        
+        public Image selectColorButtonColor;
+
+        public GameObject colorSelectionPanel;
+
+        void Start()
+        {
+            selectColorButtonColor.color = wormMaterial.color;
+        }
+
+        public void OpenColorSelectionPanel()
+        {
+            colorSelectionPanel.SetActive(true);
+        }
+
+        public void CloseColorSelectionPanel()
+        {
+            colorSelectionPanel.SetActive(false);
+        }
+        
         public void StartGame()
         {
             GameLoop.Instance.StartGame();
+        }
+
+        public void SetColor(Button button)
+        {
+            wormMaterial.color = button.image.color;
+
+            wormHeadMaterial.color = button.transform.GetChild(0).GetComponent<Image>().color;
+            
+            selectColorButtonColor.color = wormMaterial.color;
+            
+            CloseColorSelectionPanel();
         }
     }
 }
