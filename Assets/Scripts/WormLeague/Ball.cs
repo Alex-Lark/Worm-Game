0e0638bf6b46375bdff60e4bae598228302ebc23
BuildTools <unconfigured@null.spigotmc.org>
2026-03-01 02:06:01 -0800

    ball technically synced, but really weird

--- Assets/Scripts/WormLeague/Ball.cs
+++ Assets/Scripts/WormLeague/Ball.cs
@@ -1,9 +1,10 @@
  using System;
-using UnityEngine;
+ using PurrNet;
+ using UnityEngine;
 
 namespace WormLeague
 {
-    public class Ball : MonoBehaviour
+    public class Ball : NetworkBehaviour
     {
         public Player.Player LastTouchingPlayer { get; private set; }
 
@@ -21,8 +22,36 @@ namespace WormLeague
             if (collision.gameObject.CompareTag("CreaturePart") || collision.gameObject.CompareTag("WormBodySegment"))
             {
                 LastTouchingPlayer = collision.gameObject.GetComponentInParent<Player.Player>();
-                print(LastTouchingPlayer.PlayerName);
+                if (LastTouchingPlayer == null) return;
+                
+                Vector3 impulse = Vector3.zero;
+                foreach (ContactPoint contact in collision.contacts)
+                    impulse += contact.normal;
+            
+                impulse = -impulse.normalized * collision.relativeVelocity.magnitude;
+
+                // Send collision to server
+                if (isServer)
+                {
+                    ApplyCollisionForce(impulse, LastTouchingPlayer.PlayerName);
+                }
+                else
+                {
+                    ServerHandleCollision(impulse, LastTouchingPlayer.PlayerName);
+                }
             }
+        }
+        
+        [ServerRpc(requireOwnership: false)]
+        private void ServerHandleCollision(Vector3 impulse, string playerName)
+        {
+            ApplyCollisionForce(impulse, playerName);
+        }
+        
+        private void ApplyCollisionForce(Vector3 impulse, string playerName)
+        {
+            GetComponent<Rigidbody>().AddForce(impulse, ForceMode.Impulse);
+            //UpdateLastTouchingPlayer(playerName);
         }
     }
 }
