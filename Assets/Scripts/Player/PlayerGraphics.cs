13e5f1b2cf8b415a23152825f47adbc9de5c7257
Alex Lark <lark@chapman.edu>
2026-04-21 09:20:39 -0700

    fixed freezing bug here by fixing double subscribe to onscenechanged

--- Assets/Scripts/Player/PlayerGraphics.cs
+++ Assets/Scripts/Player/PlayerGraphics.cs
@@ -20,8 +20,6 @@ namespace Player
         
         void Start()
         {
-            SceneManager.sceneLoaded += OnSceneLoaded;
-
             if (GameSceneList.IsSceneAGameScene(SceneManager.GetActiveScene().name))
             {
                 EnterGameScene();
