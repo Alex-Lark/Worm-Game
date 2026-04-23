5fdcb009dbb8d99556f05252721066aa4f2760a3
Alex Lark <lark@chapman.edu>
2026-04-23 01:13:43 -0700

    headbutt and jump sound effects implemented though not networked

--- Assets/Scripts/Audio/PlayerAudio.cs
+++ Assets/Scripts/Audio/PlayerAudio.cs
@@ -6,6 +6,17 @@ namespace Audio
 {
     public class PlayerAudio : MonoBehaviour
     {
+        public AudioSource playerAudioSource;
+        public float audioSourceVolume = 0.1f;
+
+        public AudioClip jumpAudio;
+        public AudioClip headbuttChargeAudio;
+        public AudioClip headbuttLaunchAudio;
+        public AudioClip headbuttHitBall;
+        public AudioClip headbuttHitPlayer;
+        public AudioClip headbuttHitShell;
+        public AudioClip headbuttHitOther;
+        
         private Player.Player player;
         
         void Start()
@@ -58,31 +69,49 @@ namespace Audio
         private void OnWormHeadbutHitOther()
         {
             //stop whooshing headbut sound, play hitOther sound
+            playerAudioSource.volume = audioSourceVolume;
+            playerAudioSource.clip = headbuttHitOther;
+            playerAudioSource.Play();
         }
 
         private void OnWormHeadbutHitShell()
         {
             //stop whoosing headbut sound, play hitshell sound
+            playerAudioSource.volume = audioSourceVolume;
+            playerAudioSource.clip = headbuttHitShell;
+            playerAudioSource.Play();
         }
 
         private void OnWormHeadbutHitPlayer()
         {
             //stop whooshing headbut sound, play hitPlayer sound
+            playerAudioSource.volume = audioSourceVolume;
+            playerAudioSource.clip = headbuttHitPlayer;
+            playerAudioSource.Play();
         }
 
         private void OnWormHeadbutHitBall(Vector3 vector3)
         {
             //stop whooshing headbut sound, play hitBall sound
+            playerAudioSource.volume = audioSourceVolume;
+            playerAudioSource.clip = headbuttHitBall;
+            playerAudioSource.Play();
         }
 
         private void OnWormHeadbutLaunch()
         {
             //worm headbut launch sound effect
+            playerAudioSource.volume = audioSourceVolume;
+            playerAudioSource.clip = headbuttLaunchAudio;
+            playerAudioSource.Play();
         }
 
         private void OnWormHeadbutCharge()
         {
             //worm headbut charge sound effect
+            // playerAudioSource.volume = audioSourceVolume;
+            // playerAudioSource.clip = headbuttChargeAudio;
+            // playerAudioSource.Play();
         }
 
         private void OnWormForwardStart()
@@ -98,6 +127,10 @@ namespace Audio
         private void OnWormJump()
         {
             // use isWormGroundedbysegments to determine jump sound effect
+
+            playerAudioSource.volume = audioSourceVolume;
+            playerAudioSource.clip = jumpAudio;
+            playerAudioSource.Play();
         }
     }
 }
